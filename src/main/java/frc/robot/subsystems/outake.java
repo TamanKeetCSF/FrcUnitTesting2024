039929136c50c1b642d78a13136b6db55ad4d78f
notBers <109546794+notBers@users.noreply.github.com>
2024-10-05 14:18:16 -0600

    outake

--- src/main/java/frc/robot/subsystems/outake.java
+++ src/main/java/frc/robot/subsystems/outake.java
@@ -1,17 +1,34 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
-
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class outake extends SubsystemBase {
-  /** Creates a new outake. */
-  public outake() {}
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    private CANSparkMax leftMotor = new CANSparkMax(2, MotorType.kBrushless);  
+    private CANSparkMax rightMotor = new CANSparkMax(3, MotorType.kBrushless); 
+
+    public outake() {
+        leftMotor.setInverted(true);
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        
+    }
+
+    public void activateOutake() {
+        leftMotor.set(0.4);  
+        rightMotor.set(0.4);  
+    }
+
+    public void stopOutake() {
+        leftMotor.set(0.0);  
+        rightMotor.set(0.0);  
+    }
 }
