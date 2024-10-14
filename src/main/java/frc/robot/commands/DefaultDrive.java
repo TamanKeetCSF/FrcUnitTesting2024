f4a08634e847dbb5c3f5ee5beed5d3a3029fafb4
eduardo0707 <130318992+eduardo0707@users.noreply.github.com>
2024-10-14 16:38:18 -0600

    DriveTrain
    DriveTrain addition

--- src/main/java/frc/robot/commands/DefaultDrive.java
+++ src/main/java/frc/robot/commands/DefaultDrive.java
@@ -1,5 +1,60 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.commands;
 
-public class DefaultDrive {
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import java.util.function.DoubleSupplier;
 
+/**
+ * A command to drive the robot with joystick input (passed in as {@link DoubleSupplier}s). Written
+ * explicitly for pedagogical purposes - actual code should inline a command this simple with {@link
+ * edu.wpi.first.wpilibj2.command.RunCommand}.
+ */
+public class DefaultDrive extends Command {
+  private final DriveSubsystem m_drive;
+  private final DoubleSupplier m_forward;
+  private final DoubleSupplier m_rotation;
+
+  /**
+   * Creates a new DefaultDrive.
+   *
+   * @param subsystem The drive subsystem this command wil run on.
+   * @param forward The control input for driving forwards/backwards
+   * @param rotation The control input for turning
+   */
+
+  public DefaultDrive(DriveSubsystem subsystem, DoubleSupplier forward, DoubleSupplier rotation) {
+    m_drive = subsystem;
+    m_forward = forward;
+    m_rotation = rotation;
+    addRequirements(m_drive);    
+  }
+
+  //public double getLeftPower() {
+    //return m_forward.getAsDouble() + m_rotation.getAsDouble();
+//}
+
+//public double getRightPower() {
+  //  return m_forward.getAsDouble() - m_rotation.getAsDouble();
+//}
+
+
+
+  @Override
+  public void execute() {
+    double leftPower = m_forward.getAsDouble() + m_rotation.getAsDouble()  ;
+    double rightPower = m_forward.getAsDouble() - m_rotation.getAsDouble()  ;
+    
+
+        // Adding a range of the joysticks in which the robot will not respond
+        leftPower = (Math.abs(leftPower) <  0.07)? 0 : leftPower;
+        rightPower = (Math.abs(rightPower) <  0.07)? 0 : rightPower;
+        
+        RobotContainer.m_robotDrive.setMotorsRaw(leftPower, rightPower);
+  }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,5 +1,71 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems;
 
-public class DriveSubsystem {
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+//import frc.robot.Constants.DriveConstants;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+
+public class DriveSubsystem extends SubsystemBase {
+  // The motors on the left side of the drive.
+  private CANSparkMax m_leftLeader = new CANSparkMax(9, MotorType.kBrushless);
+  private CANSparkMax m_leftFollower = new CANSparkMax(8, MotorType.kBrushless);
+  //private final TalonSRX m_leftLeader = new TalonSRX(11);
+  //private final TalonSRX m_leftFollower = new TalonSRX(9);
+
+  // The motors on the right side of the drive.
+  private CANSparkMax m_rightLeader = new CANSparkMax(3, MotorType.kBrushless);
+  private CANSparkMax m_rightFollower = new CANSparkMax(4, MotorType.kBrushless);
+
+  //private final TalonSRX m_rightLeader = new TalonSRX(13);
+  //private final TalonSRX m_rightFollower = new TalonSRX(12);
+
+
+  
+	public void setMotors(double left, double right) {
+    left = scaleLeft(left);
+    right = scaleRight(right);
+    
+    setMotorsRaw(left, right);
+  }
+  
+  public void setMotorsRaw(double left, double right) {
+    double velocidadleft = safetyTest(left);
+    double velocidadright = safetyTest(right);
+    
+    m_leftLeader.set(-velocidadleft);
+    m_leftFollower.set(-velocidadleft);
+    m_rightLeader.set(velocidadright);	
+    m_rightFollower.set( velocidadright);
+    System.out.println("left: " + velocidadleft + "right" + velocidadright);
+}
+  
+  private double safetyTest(double motorValue) {
+      motorValue = (motorValue < -1) ? -1 : motorValue;
+      motorValue = (motorValue > 1) ? 1 : motorValue;
+      
+      return motorValue;
+  }
+  
+  private double scaleLeft(double left) {
+    return 0.5 * left;
+  }
+  
+  private double scaleRight(double right) {
+    return 0.5 * right;
+  }
+
 
 }
